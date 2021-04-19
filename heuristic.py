349bd34f9bdce2bce9ba2ce350691a7977f9f280
liyeouwang <liwen4710@gmail.com>
2021-04-19 20:24:03 +0800

    ideal scheduling

--- heuristic.py
+++ heuristic.py
@@ -61,6 +61,18 @@ class Request:
 		self.ideal_slots = [0] * num_servers
 		self.earliest_start_time = 0 # = T_request
 
+def check_freshness(request, j, jj):
+	if(services[request.service_type].T_compute[j] + services[request.service_type].T_deliver[j][jj] <= freshness):
+		return True
+	else:
+		return False
+
+def check_deadline(request, time):
+	if(time <= request.deadline):
+		return True
+	else:
+		return False
+
 def read_inputs():
 
 	num_vehicles = int(input())
@@ -114,7 +126,6 @@ def read_inputs():
 				requests.append(Request(i, k, num_requests, num_servers))
 				num_requests += 1
 
-	print(len(requests))
 	# Ideal time slots of each task on each server
 	for r in range(len(requests)):
 		for j in range(len(servers)):
@@ -134,13 +145,10 @@ def read_inputs():
 			interval = interval.split()
 			period = int(interval[0])
 			server_id = int(interval[1])
-			#servers[server_id].vehicle_in[i].append(travel_time)
-			#servers[server_id].vehicle_out[i].append(travel_time + period - 1)
 			vehicles[i].servers.append(server_id)
 			vehicles[i].servers_in.append(travel_time)
 			vehicles[i].servers_out.append(travel_time + period - 1)
 			travel_time = travel_time + period
-	#sys.exit()
 
 	# Memory size of each service 
 	sizes = [int(x) for x in input().split()]
@@ -158,6 +166,77 @@ def read_inputs():
 		request.size = services[request.service_type].size
 
 
+def fit(ideal_slot, request, server):
+	is_scheduled = False
+	T_compute = services[request.service_type].T_compute[server.server_id]
+
+	for slot in server.slots:
+		if(slot.start >= ideal_slot.start and slot.end <= ideal_slot.end):
+			if(slot.end - slot.start >= T_compute):
+				is_scheduled = True
+				request.start_exe_time = slot.end - T_compute
+				new_slot = Slot(slot.start, request.start_exe_time)
+				slot.start = request.start_exe_time + T_compute + 1
+				if(new_slot.duration > 0):
+					server.slots.insert(server.slots.index(slot)+1, new_slot)
+				if(slot.duration <= 0):
+					server.slots.remove(slot)
+				break
+
+		if(slot.start >= ideal_slot.start and slot.start <= ideal_slot.end and slot.end >= ideal_slot.end):
+			if(ideal_slot.end - slot.start >= T_compute):
+				is_scheduled = True
+				request.start_exe_time = slot.start
+				slot.start = request.start_exe_time + T_compute + 1
+				if(slot.duration <= 0):
+					server.slots.remove(slot)
+				break
+
+		if(slot.start <= ideal_slot.start and slot.end >= ideal_slot.start and slot.end <= ideal_slot.end):
+			if(slot.end - ideal_slot.start >= T_compute):
+				is_scheduled = True
+				request.start_exe_time = slot.end - T_compute
+				slot.end = request.start_exe_time
+				if(slot.duration <= 0):
+					server.slots.remove(slot)
+				break
+
+		if(slot.start <= ideal_slot.start and slot.end >= ideal_slot.end):
+			is_scheduled = True
+			request.start_exe_time = ideal_slot.end - T_compute
+			new_slot = Slot(slot.start, request.start_exe_time)
+			slot.start = request.start_exe_time + T_compute + 1
+			if(new_slot.duration > 0):
+				server.slots.insert(server.slots.index(slot)+1, new_slot)
+			if(slot.duration <= 0):
+				server.slots.remove(slot)
+			break
+
+	return is_scheduled
+
+def ideal_scheduling(request, server_loading_list):
+	for s in server_loading_list:
+		S_exe = s[0]
+		for index in range(len(vehicles[request.vehicle_id].servers)):
+			S_deliver = vehicles[request.vehicle_id].servers[index]
+			T_compute = services[request.service_type].T_compute[S_exe]
+			T_deliver = services[request.service_type].T_deliver[S_exe][S_deliver]
+			T_request = services[request.service_type].T_request[S_exe]
+			t_in = vehicles[request.vehicle_id].servers_in[index]
+			t_out = vehicles[request.vehicle_id].servers_out[index]
+			if(T_compute + T_deliver <= request.freshness and t_in <= request.deadline):
+				start = max(T_request, t_in - T_deliver - T_compute)
+				end = min(request.deadline - T_deliver, t_out - T_deliver)
+				if((end - start > T_compute) and (fit(Slot(start, end), request, servers[S_exe]) == True)):
+					request.S_exe = S_exe
+					request.S_deliver = S_deliver
+					request.T_compute = T_compute
+					request.T_deliver = T_deliver
+					request.catch_time = request.start_exe_time + T_compute + T_deliver
+					return True
+
+
+
 if __name__ == '__main__':
 
 	servers = []	
@@ -168,5 +247,22 @@ if __name__ == '__main__':
 	# Read input file and initialize parameters
 	read_inputs()
 
-
-	possible_exe_servers = []
+	# Initialize server-loading list
+	server_loading_list = []
+	#server_loading_dict = dict()
+	for server in servers:
+		#server_loading_dict.update({server.server_id: server.loading})
+		server_loading_list.append((server.server_id, server.loading))
+	
+	# Schedule tasks
+	# Try to schedule the task in an ideal slot 
+	# If it can't be scheduled in any ideal slot, schedule it in normal slot. 
+	for request in requests:
+		is_scheduled = ideal_scheduling(request, server_loading_list)
+		sys.exit()
+		if(is_scheduled == True):	
+			#update_server_loading(request, server_loading_list)
+			continue
+		#else:
+			#non_ideal_scheduling(request, server_loading_list)
+			#update_server_loading(request, server_loading_list)
